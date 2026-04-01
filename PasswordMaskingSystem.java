7a6642267cdca6c230504aaf98e1ddd2c10aac4b
pranali-b26 <pranalibarsagade04@gmail.com>
2026-04-01 23:01:50 +0530

    PasswordMaskinSystem

---
+++ PasswordMaskingSystem.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class PasswordMaskingSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter password: ");
+        String pass = sc.nextLine();
+
+        String masked = pass.substring(0, pass.length()-2).replaceAll(".", "*")
+                        + pass.substring(pass.length()-2);
+
+        System.out.println("Masked Password: " + masked);
+    }
+}
