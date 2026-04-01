5dc7cbcef7c552085969ad93c4cd6012c6b02a7e
pranali-b26 <pranalibarsagade04@gmail.com>
2026-04-01 09:01:43 +0530

    TextProcessingBuilder file added successfully

---
+++ TextProcessingBuilder.java
@@ -0,0 +1,9 @@
+class TextProcessingBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+
+        sb.append(" Java");
+
+        System.out.println("Modified: " + sb);
+    }
+}
